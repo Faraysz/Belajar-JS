6325f4f4a5c0ac09ba58f152eb890cfcede1b021
Faraytodi Atmajaya <150215021+Faraysz@users.noreply.github.com>
2025-05-02 21:46:09 +0700

    update main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const s = 'Hello World! This is a test alert from the main.js file.';
+
+console.log(s.toUpperCase()); // Convert to uppercase
+
+// var, let, const 
+
+// console.log( 'Hello World! This is a test alert from the main.js file.');
+// console.error( 'This is an error message from the main.js file.');
+// console.warn( 'This is a warning message from the main.js file.');
+
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// const name = 'John Doe'; // String
+// const age = 30; // Number
+
+// // concatenation
+// console.log('My name is ' +  name + ' and I am ' + age + ' years old.');
+// // template string
+// const hello = `My name is ${name} and I am ${age} years old.`;
+
+// console.log(hello);
+
+// const name = 'John Doe'; // String
+// const age = 30; // Number
+// const rating = 4.5; // Number (float)
+// const isCool = true; // Boolean
+// const x = null; // Null
+// const y = undefined; // Undefined
+// let z; // Undefined (not initialized)
+
+// console.log(typeof z); // String
